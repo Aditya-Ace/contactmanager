ad1a41d9f9d2a94454397e1391a4554bd5ea2a75
Aditya-Ace <aditya.ace@hotmail.com>
2018-12-26 07:10:11 +0530

    Updates

---
+++ precache-manifest.fd8f72cdc707f5f91766fc99dfba2d0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3a126f89a4f7045c744",
+    "url": "/contactmanager/static/css/main.a41f0652.chunk.css"
+  },
+  {
+    "revision": "e3a126f89a4f7045c744",
+    "url": "/contactmanager/static/js/main.e3a126f8.chunk.js"
+  },
+  {
+    "revision": "2c6398a7c7ef810923a6",
+    "url": "/contactmanager/static/css/1.400de841.chunk.css"
+  },
+  {
+    "revision": "2c6398a7c7ef810923a6",
+    "url": "/contactmanager/static/js/1.2c6398a7.chunk.js"
+  },
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "b4a6035fed77386038d518fd5bbfe9b3",
+    "url": "/contactmanager/index.html"
+  }
+];
